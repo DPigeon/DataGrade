7950cee22f242bbb3de976156f5c0cb32f5326b1
DPigeon <D_Pig@encs.concordia.ca>
2020-02-03 21:48:20 -0500

    Fix width of fragment & database variables

---
+++ app/src/main/java/com/example/utilisateur/assignment2/DatabaseConfig.java
@@ -0,0 +1,23 @@
+package com.example.utilisateur.assignment2;
+
+/*
+* Database Variables to keep
+*/
+
+public class DatabaseConfig {
+    // Database
+    public static final String DATABASE_NAME = "course_database";
+
+    // Course Table
+    public static final String COURSE_TABLE = "course_table";
+    public static final String COLUMN_COURSE_ID = "course_id";
+    public static final String COLUMN_COURSE_TITLE = "course_title";
+    public static final String COLUMN_COURSE_CODE = "course_code";
+
+    // Assignment Table
+    public static final String ASSIGNMENT_TABLE = "assignment_table";
+    public static final String COLUMN_ASSIGNMENT_ID = "assignment_id";
+    public static final String COLUMN_ASSIGNMENT_COURSE_ID = "assignment_course_id";
+    public static final String COLUMN_ASSIGNMENT_TITLE = "assignment_title";
+    public static final String COLUMN_ASSIGNMENT_GRADE = "assignment_id";
+}
