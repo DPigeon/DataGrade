403c41e3674245b60544b5fd5b7058c6ffd1af08
DPigeon <D_Pig@encs.concordia.ca>
2020-02-03 22:21:58 -0500

    Assignment Model

---
+++ app/src/main/java/com/example/utilisateur/assignment2/Assignment.java
@@ -0,0 +1,53 @@
+package com.example.utilisateur.assignment2;
+
+public class Assignment {
+    int id;
+    int courseId;
+    String title;
+    double grade;
+
+    Assignment(int i, int cId, String name, double percent) {
+        id = i;
+        courseId = cId;
+        title = name;
+        grade = percent;
+    }
+
+    // Getters
+    public int getId() {
+        return id;
+    }
+
+    public int getCourseId() {
+        return courseId;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public double getGrade() {
+        return grade;
+    }
+
+    public String getInfo() { // Will be used as a toast
+        return this.title  + " has been created!";
+    }
+
+    //Setters
+    public void setId(int i) {
+        id = i;
+    }
+
+    public void setCourseId(int i) {
+        courseId = i;
+    }
+
+    public void setTitle(String name) {
+        title = name;
+    }
+
+    public void setGrade(double percent) {
+        grade = percent;
+    }
+}
