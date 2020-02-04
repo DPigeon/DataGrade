5520d60e4884b86d8713740996adfa7ab8356a39
DPigeon <D_Pig@encs.concordia.ca>
2020-02-03 20:00:49 -0500

    Defining the UI in main activity

--- app/src/main/java/com/example/utilisateur/assignment2/MainActivity.java
+++ app/src/main/java/com/example/utilisateur/assignment2/MainActivity.java
@@ -3,12 +3,30 @@ package com.example.utilisateur.assignment2;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class MainActivity extends AppCompatActivity {
+
+    protected FloatingActionButton floatingActionButton;
+    protected TextView coursesTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        coursesTextView = findViewById(R.id.coursesTextView);
+        floatingActionButton = findViewById(R.id.floatingActionButton);
+
+        floatingActionButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                
+            }
+        });
+
     }
 }
